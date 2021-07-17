592bbdfe3065921a498538871b9ee9070f92b996
Jenny Lim <z5205161@ad.unsw.edu.au>
2021-07-17 14:03:44 +1000

    set up react

---
+++ hackathon/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h>
+          Welcome to our page
+        </h>
+      </header>
+    </div>
+  );
+}
+
+export default App;
